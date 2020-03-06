6a3d861bf6f6720e736c55d3c0fba36ed1df5382
Lokie89 <traeuman@gmail.com>
2020-03-06 17:04:51 +0900

    baseballversion1-2

--- src/m01baseball/Main.java
+++
@@ -1,9 +0,0 @@
-package m01baseball;
-
-import m01baseball.game.baseball.BaseBallGame;
-
-public class Main {
-    public static void main(String[] args) {
-        new BaseBallGame().play();
-    }
-}
